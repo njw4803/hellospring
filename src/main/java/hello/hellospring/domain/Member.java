8145c0025749082cc3be001b3aba5007ec4215a4
nojiwon1227@gmail.com <nojiwon1227@gmail.com>
2023-02-26 16:52:58 +0900

    spring JdbcTemplate

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,19 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY DB가 자동으로 생성해줌
     private Long id;
+
+    //@Column(name = "username") // 컬럼이름 값을 넣어주면 된다.
     private String name;
 
     public Long getId() {
